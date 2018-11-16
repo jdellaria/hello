214d71b2007407c1328a191e4476cad37d82bda9
Jon <jdellaria@gmail.com>
2018-11-16 10:41:50 -0500

    First Commit

---
+++ src/hello.cpp
@@ -0,0 +1,36 @@
+//============================================================================
+// Name        : hello.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+
+#include <DLog.h>
+#include <stdio.h>
+#include <iostream>
+#include <time.h>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	DLog myLog;
+	string message;
+
+//	myLog.setLogFileName ("jon.txt");
+//	myLog.setErrorLevel(logInformation);
+	myLog.logValue = logInformation;
+	myLog.logFileName = "/home/jdellaria/Desktop/log.txt";
+	myLog.printFile = true;
+	myLog.printScreen = true;
+	myLog.printTime = true;
+
+	message = "this is my Log message ";
+//	message += std::to_string(100);
+
+	myLog.print(logInformation, (string)"this is my message");
+
+	myLog.print(logInformation, message);
+	return 0;
+}
